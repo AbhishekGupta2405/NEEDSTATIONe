b31298d678c1867e6edd051ac5888b5ce72f1919
AbhishekGupta2405 <abhishek.gupta@crackquant.com>
2025-03-01 20:54:25 +0530

    url changed

--- Frontend/Need_Station_MP-main/src/pages/SignupPage/Signup.jsx
+++ Frontend/Need_Station_MP-main/src/pages/SignupPage/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
 
   const handleSignup = async () => {
     try {
-      const response = await fetch('http://localhost:8080/api/auth/register', {
+      const response = await fetch('http://localhost:8080/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
