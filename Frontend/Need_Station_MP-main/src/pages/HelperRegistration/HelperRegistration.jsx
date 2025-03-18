326c39bb88ec3435d38b82bc120a7566601bc92b
AbhishekGupta2405 <abhishek2405gupta@gmail.com>
2025-03-18 19:43:53 +0530

    merged helper side pages into main repo

--- Frontend/Need_Station_MP-main/src/pages/HelperRegistration/HelperRegistration.jsx
+++ Frontend/Need_Station_MP-main/src/pages/HelperRegistration/HelperRegistration.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./HelperRegistration.module.css";
 import ScrollToTop from "../../hooks/ScrollToTop.jsx";
+import { Link } from "react-router-dom";
 
 const HelperRegistration = () => {
   const [formData, setFormData] = useState({
@@ -35,160 +36,186 @@ const HelperRegistration = () => {
 
   return (
     <>
-    <ScrollToTop/>
-    <main>
-      <div className={styles["container"]}>
-        <form className={styles["registration-form"]} onSubmit={handleSubmit}>
-          <h2>Register as a Worker</h2>
+      <ScrollToTop />
+      <main>
+        <div className={styles["container"]}>
+          <form className={styles["registration-form"]} onSubmit={handleSubmit}>
+            <h2>Register as a Worker</h2>
 
-          {/* Personal Information */}
-          <div className={styles["form-section"]}>
-            <h3>Personal Information</h3>
-            <label htmlFor="fullName">Full Name</label>
-            <input
-              type="text"
-              id="fullName"
-              name="fullName"
-              placeholder="Enter your full name"
-              value={formData.fullName}
-              onChange={handleChange}
-              required
-            />
+            {/* Personal Information */}
+            <div className={styles["form-section"]}>
+              <h3>Personal Information</h3>
+              <label htmlFor="fullName">Full Name</label>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                placeholder="Enter your full name"
+                value={formData.fullName}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="dob">Date of Birth</label>
-            <input
-              type="date"
-              id="dob"
-              name="dob"
-              value={formData.dob}
-              onChange={handleChange}
-              required
-            />
+              <label htmlFor="dob">Date of Birth</label>
+              <input
+                type="date"
+                id="dob"
+                name="dob"
+                value={formData.dob}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="gender">Gender</label>
-            <select
-              id="gender"
-              name="gender"
-              value={formData.gender}
-              onChange={handleChange}
-              required
-            >
-              <option className={styles["option"]} value="">Select Gender</option>
-              <option className={styles["option"]} value="male">Male</option>
-              <option className={styles["option"]} value="female">Female</option>
-              <option className={styles["option"]} value="other">Other</option>
-            </select>
+              <label htmlFor="gender">Gender</label>
+              <select
+                id="gender"
+                name="gender"
+                value={formData.gender}
+                onChange={handleChange}
+                required
+              >
+                <option className={styles["option"]} value="">
+                  Select Gender
+                </option>
+                <option className={styles["option"]} value="male">
+                  Male
+                </option>
+                <option className={styles["option"]} value="female">
+                  Female
+                </option>
+                <option className={styles["option"]} value="other">
+                  Other
+                </option>
+              </select>
 
-            <label htmlFor="phone">Phone Number</label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              placeholder="Enter your phone number"
-              value={formData.phone}
-              onChange={handleChange}
-              required
-            />
+              <label htmlFor="phone">Phone Number</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="Enter your phone number"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="familyPhone">Family Phone Number</label>
-            <input
-              type="tel"
-              id="familyPhone"
-              name="familyPhone"
-              placeholder="Enter your family phone number"
-              value={formData.familyPhone}
-              onChange={handleChange}
-              required
-            />
-          </div>
+              <label htmlFor="familyPhone">Family Phone Number</label>
+              <input
+                type="tel"
+                id="familyPhone"
+                name="familyPhone"
+                placeholder="Enter your family phone number"
+                value={formData.familyPhone}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-          {/* Address Information */}
-          <div className={styles["form-section"]}>
-            <h3>Address Information</h3>
-            <label htmlFor="address">Permanent Address</label>
-            <textarea
-              id="address"
-              name="address"
-              rows="4"
-              placeholder="Enter your permanent address"
-              value={formData.address}
-              onChange={handleChange}
-              required
-            ></textarea>
+            {/* Address Information */}
+            <div className={styles["form-section"]}>
+              <h3>Address Information</h3>
+              <label htmlFor="address">Permanent Address</label>
+              <textarea
+                id="address"
+                name="address"
+                rows="4"
+                placeholder="Enter your permanent address"
+                value={formData.address}
+                onChange={handleChange}
+                required
+              ></textarea>
 
-            <label htmlFor="currentAddress">Current Address (optional)</label>
-            <textarea
-              id="currentAddress"
-              name="currentAddress"
-              rows="4"
-              placeholder="Enter your current address"
-              value={formData.currentAddress}
-              onChange={handleChange}
-            ></textarea>
-          </div>
+              <label htmlFor="currentAddress">Current Address (optional)</label>
+              <textarea
+                id="currentAddress"
+                name="currentAddress"
+                rows="4"
+                placeholder="Enter your current address"
+                value={formData.currentAddress}
+                onChange={handleChange}
+              ></textarea>
+            </div>
 
-          {/* Identity Verification */}
-          <div className={styles["form-section"]}>
-            <h3>Identity Verification</h3>
-            <label htmlFor="aadhaar">Aadhaar Number</label>
-            <input
-              type="text"
-              id="aadhaar"
-              name="aadhaar"
-              placeholder="Enter your Aadhaar number"
-              value={formData.aadhaar}
-              onChange={handleChange}
-              required
-            />
+            {/* Identity Verification */}
+            <div className={styles["form-section"]}>
+              <h3>Identity Verification</h3>
+              <label htmlFor="aadhaar">Aadhaar Number</label>
+              <input
+                type="text"
+                id="aadhaar"
+                name="aadhaar"
+                placeholder="Enter your Aadhaar number"
+                value={formData.aadhaar}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="verification">Police Verification Status</label>
-            <select
-              id="verification"
-              name="verification"
-              value={formData.verification}
-              onChange={handleChange}
-              required
-            >
-              <option className={styles["option"]} value="">Select Status</option>
-              <option className={styles["option"]} value="verified">Verified</option>
-              <option className={styles["option"]} value="pending">Pending</option>
-            </select>
-          </div>
+              <label htmlFor="verification">Police Verification Status</label>
+              <select
+                id="verification"
+                name="verification"
+                value={formData.verification}
+                onChange={handleChange}
+                required
+              >
+                <option className={styles["option"]} value="">
+                  Select Status
+                </option>
+                <option className={styles["option"]} value="verified">
+                  Verified
+                </option>
+                <option className={styles["option"]} value="pending">
+                  Pending
+                </option>
+              </select>
+            </div>
 
-          {/* Employment Details */}
-          <div className={styles["form-section"]}>
-            <h3>Employment Details</h3>
-            <label htmlFor="category">Work Category</label>
-            <select
-              id="category"
-              name="category"
-              value={formData.category}
-              onChange={handleChange}
-              required
-            >
-              <option className={styles["option"]} value="">Select Category</option>
-              <option className={styles["option"]} value="elder_care">Elder Care</option>
-              <option className={styles["option"]} value="babysitter">Babysitter</option>
-              <option className={styles["option"]} value="maid">Maid</option>
-              <option className={styles["option"]} value="electrician">Electrician</option>
-              <option className={styles["option"]} value="plumber">Plumber</option>
-            </select>
+            {/* Employment Details */}
+            <div className={styles["form-section"]}>
+              <h3>Employment Details</h3>
+              <label htmlFor="category">Work Category</label>
+              <select
+                id="category"
+                name="category"
+                value={formData.category}
+                onChange={handleChange}
+                required
+              >
+                <option className={styles["option"]} value="">
+                  Select Category
+                </option>
+                <option className={styles["option"]} value="elder_care">
+                  Elder Care
+                </option>
+                <option className={styles["option"]} value="babysitter">
+                  Babysitter
+                </option>
+                <option className={styles["option"]} value="maid">
+                  Maid
+                </option>
+                <option className={styles["option"]} value="electrician">
+                  Electrician
+                </option>
+                <option className={styles["option"]} value="plumber">
+                  Plumber
+                </option>
+              </select>
 
-            <label htmlFor="experience">Years of Experience</label>
-            <input
-              type="number"
-              id="experience"
-              name="experience"
-              min="0"
-              placeholder="Enter your years of experience"
-              value={formData.experience}
-              onChange={handleChange}
-              required
-            />
-          </div>
+              <label htmlFor="experience">Years of Experience</label>
+              <input
+                type="number"
+                id="experience"
+                name="experience"
+                min="0"
+                placeholder="Enter your years of experience"
+                value={formData.experience}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-          {/* Bank Details */}
-          {/* <div className={styles["form-section"]}>
+            {/* Bank Details */}
+            {/* <div className={styles["form-section"]}>
             <h3>Banking Information</h3>
             <label htmlFor="bankAccount">Bank Account Number</label>
             <input
@@ -223,37 +250,43 @@ const HelperRegistration = () => {
             />
           </div> */}
 
-          {/* Emergency Contact */}
-          <div className={styles["form-section"]}>
-            <h3>Emergency Contact</h3>
-            <label htmlFor="emergencyContact">Emergency Contact Name</label>
-            <input
-              type="text"
-              id="emergencyContact"
-              name="emergencyContact"
-              placeholder="Enter emergency contact name"
-              value={formData.emergencyContact}
-              onChange={handleChange}
-              required
-            />
+            {/* Emergency Contact */}
+            <div className={styles["form-section"]}>
+              <h3>Emergency Contact</h3>
+              <label htmlFor="emergencyContact">Emergency Contact Name</label>
+              <input
+                type="text"
+                id="emergencyContact"
+                name="emergencyContact"
+                placeholder="Enter emergency contact name"
+                value={formData.emergencyContact}
+                onChange={handleChange}
+                required
+              />
 
-            <label htmlFor="emergencyPhone">Emergency Contact Number</label>
-            <input
-              type="tel"
-              id="emergencyPhone"
-              name="emergencyPhone"
-              placeholder="Enter emergency contact number"
-              value={formData.emergencyPhone}
-              onChange={handleChange}
-              required
-            />
-          </div>
+              <label htmlFor="emergencyPhone">Emergency Contact Number</label>
+              <input
+                type="tel"
+                id="emergencyPhone"
+                name="emergencyPhone"
+                placeholder="Enter emergency contact number"
+                value={formData.emergencyPhone}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-          {/* Submit Button */}
-          <button className={styles["button"]} type="submit">Register Now</button>
-        </form>
-      </div>
-    </main> </>
+            {/* Submit Button */}
+
+            <Link to="/helper/overview">
+              <button className={styles["button"]} type="button">
+                Register
+              </button>
+            </Link>
+          </form>
+        </div>
+      </main>{" "}
+    </>
   );
 };
 
